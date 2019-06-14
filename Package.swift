f14f56c95fcdbc5c29fbbcc959b3f7374a92ca4e
Samantha Gatt <smfgatt@gmail.com>
2019-06-14 13:36:52 -0500

    Adds dependencies

--- Package.swift
+++ Package.swift
@@ -6,9 +6,15 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", .upToNextMinor(from: "3.0.0")),
+        // Fluent driver for SQLite
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMinor(from: "3.0.0")),
+        // 🔑 Hashing (BCrypt, SHA2, HMAC), encryption (AES), public-key (RSA), and random data generation.
+        .package(url: "https://github.com/vapor/crypto.git", .upToNextMinor(from: "3.3.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentSQLite", "Crypto"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
